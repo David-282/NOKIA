2b7a04a2f96ac97626edd4aff420e92a141538b1
David-282 <afolabidavid94@gmail.com>
2025-11-15 14:37:03 +0100

    Complete Nokia Task

---
+++ NokiaComplete.java
@@ -0,0 +1,211 @@
+import java.util.Scanner;
+     public class NokiaComplete{
+     public static void main(String[]args){
+
+
+         Scanner input = new Scanner(System.in);     
+          System.out.println("""
+         
+         ........Menu......
+          1. Phonebook
+          2. Messages
+          3. Chats
+          4. Call Register
+          5. Tones
+          6. Settings
+          7. Call divert
+          8. Games
+          9. Calculator
+         10. Reminders
+         11. Clock
+         12. Profiles
+         13. SIM services3  """);
+
+          System.out.print("Select from the options avaliable: ");
+          int menu = input.nextInt();
+
+          switch (menu){ 
+          case 1-> {
+                  System.out.print("""
+          ........Phonebook.........
+               1. Search
+               2. Service Nos. 
+               3. Add name
+               4. Erase
+               5. Edit
+               6. Assign tone
+               7. Send b’card
+               8. Options
+               9. Speed dials
+              10. Voice tags
+           Select from the options avaliable:  """);
+                             
+               int phonebook = input.nextInt();
+               switch(phonebook){
+               case 8-> System.out.print("""
+                     ........options........
+                         1. Type of view
+                         2. Memory status\n """);                             
+                             }
+                         }
+          case 2-> {
+                    System.out.print("""
+           ........Messages..........
+                         
+           1. Write messages
+           2. Inbox
+           3. Outbox
+           4. Picture messages
+           5. Templates
+           6. Smileysz
+           7. Message settings
+           8. Info service
+           9. Voice mailbox number
+          10.Service command editor
+       Select from the options avaliable: """);
+          
+
+               int messages =input.nextInt();
+               switch(messages){
+               case 7->{ 
+                          System.out.print("""
+                ..........Messages settings........         
+                           1. Set 12
+                           2. Common 
+             Select from the options avaliable:    """);
+
+
+                            int messageSet = input.nextInt();
+                            switch (messageSet){
+                              case 1->System.out.println("""
+                                       1. Message centre number
+                                       2. Messages sent as
+                                       3. Message validity  """);
+                              case 2-> System.out.println("""
+                                           1. Delivery reports
+                                           2. Reply via same centre
+                                           3. Character support """);
+                                 }
+                              }
+                         }                  
+                     }
+          case 4->{
+                    System.out.print("""
+               .......Call Register......
+                    1. Missed calls
+                    2. Received calls
+                    3. Dialled numbers
+                    4. Erase recent call lists
+                    5. Show call duration 
+                    6. Show call costs
+                    7. Call cost settings
+                    8. Prepaid credit
+              Select from the options avaliable:    """);
+
+                         int callRegister = input.nextInt();
+                         switch (callRegister) {
+                         case 5 -> System.out.println("""
+                         ........Show call duration.......
+                              1. Last call duration
+                              2. All calls’ duration
+                              3. Received calls’ duration
+                              4. Dialled calls’ d
+                              5. Clear timers   """);
+
+                         case 6-> System.out.println("""
+                        ........Show call costs........
+                              1. Last call cost
+                              2. All calls’ cost
+                              3. Clear counters  """);
+
+                         case 7 ->System.out.println("""
+                           ........ Call cost settings......
+                              1. Call cost limit
+                              2. Show costs in  """);
+
+                              }
+                        }
+                 
+          case 5-> {
+                    System.out.println("""
+                   ......Tones.....
+                    1. Ringing tone
+                    2. Ringing volume
+                    3. Incoming call alert
+                    4. Composer
+                    5. Message alert tone
+                    6. Keypad tones
+                    7. Warning and game tones
+                    8. Vibrating alert
+                    9. Screen saver    """);
+                     
+                         }
+          case 6 ->{
+                System.out.print("""
+               ........Settings.......
+                    1. Call settings
+                    2. Phone settings
+                    3. Security settings
+                    4. Restore factory settings
+                  Select from the options avaliable:   """);
+
+
+                           int settings = input.nextInt();
+                        switch (settings){
+                         case 1 ->{
+                          System.out.println("""
+                          .....Call Settings.....
+                         1. Automatic redial
+                         2. Speed dialling
+                         3. Call waiting options
+                         4. Own number sending
+                         5. Phone line in use
+                         6. Automatic answer  """);
+                                   }  
+
+                         case 2 -> {
+                           System.out.println("""
+                          .......Phone Settings....
+                         1. Language
+                         2. Cell info display
+                         3. Welcome note
+                         4. Network selection
+                         5. Lights2
+                         6. Confirm SIM service actions  """);
+                              }
+
+                         case 3 -> {
+                           System.out.println("""
+                            ......Security settings......
+                          1. PIN code request
+                          2. Call barring service
+                          3. Fixed dialling
+                          4. Closed user group
+                          5. Phone security
+                          6. Change access codes """);
+                                      }
+                           }  
+                      }      
+
+
+          case 11-> {
+               System.out.println("""
+               .......Clock.....
+               1. Alarm clock
+               2. Clock settings
+               3. Date setting
+               4. Stopwatch
+               5. Countdown timer
+               6. Auto update of date and time   """);
+                             }          
+
+
+
+               }
+
+     }
+
+}
+
+
+
