dc6d6d748d5264c22b0ccd83dee9bc9118c6e2b7
David Afolabi <afolabidavid94@gmail.com>
2026-01-21 00:18:32 +0100

    Delete Documents directory

--- Documents/Assigmnent Submission/MR CHI/PrintTable.py
+++
@@ -1,3 +0,0 @@
-for num in range(1, 6):  
-    print(num, num + 1, int(pow(num, num + 1)))
-
--- Documents/JAVA/PayrollCalculation.java
+++
@@ -1,6 +0,0 @@
-public class PayrollCalculation{
-public static void main(String[]args){
-System.out.println("Value  2*8");
-System.out.printf("%d is not equal to %d\n", (x + y), (x * y) );
-}
-}
